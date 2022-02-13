94a50c997276396d9f9538eacc4fbb53b47d4bba
Argenis De La Rosa <oftherose91@gmail.com>
2022-02-13 11:07:11 -0500

    Initial Commit

---
+++ FirebaseSPM/FirebaseSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSPMApp.swift
+//  FirebaseSPM
+//
+//  Created by Argenis De La Rosa on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
